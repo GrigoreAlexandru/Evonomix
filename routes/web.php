0cd2ad9dd0ef6533761ceefa01b663be83e15e94
Alexandru <GrigoreAlexandru.dev@gmail.com>
2019-06-19 16:19:21 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,4 @@ Route::get('/', 'WelcomeController@index')->name('index');
 Auth::routes(['verify' => true]);
 
 // acces numai pt useri cu email verificat
-//Route::get('/content', 'ScheduleContentsController@index')
-//    ->name('content.index')
-//    ->middleware('verified');
-//
-//Route::get('/content/create', 'ScheduleContentsController@create')
-//    ->name('content.create')
-//    ->middleware('verified');
-//
-//Route::get('/content/{id}/edit', 'ScheduleContentsController@edit')
-//    ->name('content.edit')
-//    ->middleware('verified');
-//
-//Route::post('/content/{id}', 'ScheduleContentsController@update')
-//    ->name('content.update')
-//    ->middleware('verified');
-//
-//Route::post('/content', 'ScheduleContentsController@store')
-//    ->name('content.store')
-//    ->middleware('verified');
 Route::resource('content', 'ScheduleContentsController', ['except' => 'show'])->middleware('verified');
